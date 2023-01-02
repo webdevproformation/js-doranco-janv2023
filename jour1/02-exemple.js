e09c9cfe208f6ab5c6168dcbc5bf405ccb7e5617
Malik H <malik.h@webdevpro.net>
2023-01-02 16:36:18 +0100

    start

---
+++ jour1/02-exemple.js
@@ -0,0 +1,9 @@
+// écrit directement du js 
+// pas besoin d'écrire <script></script>
+/**
+ * commentaire
+ * multiligne 
+ * ne pas écrire directement du html dans ce fichier 
+ */
+
+// 03-variable.html
